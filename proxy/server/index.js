9040e87e0ed8bda6c9b5456f3342a5f0d17f0712
dannyhannyford <dannyhannyford@gmail.com>
2020-10-08 16:53:06 -0700

    full amazon ipv4 dns address with open port and req.url

--- proxy/server/index.js
+++ proxy/server/index.js
@@ -12,9 +12,33 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-  axios.get('http://34.208.9.166//api/listings')
+app.get('/api/listings', (req, res) => {
+  axios.get(`http://ec2-34-208-9-166.us-west-2.compute.amazonaws.com:80${req.url}`)
     .then(({ data }) => {
+      res.send(data);
+    })
+    .catch(err => console.log(err));
+});
+app.get('/api/reviews/:listing_id', (req, res) => {
+  axios.get(`http://ec2-18-237-166-117.us-west-2.compute.amazonaws.com:80${req.url}`)
+    .then(({ data }) => {
+      console.log('reviews data', data);
+      res.send(data);
+    })
+    .catch(err => console.log(err));
+});
+app.get('/api/images/:listing_id', (req, res) => {
+  axios.get(`http://ec2-18-217-241-92.us-west-2.compute.amazonaws.com:80${req.url}`)
+    .then(({ data }) => {
+      console.log('images data', data);
+      res.send(data);
+    })
+    .catch(err => console.log(err));
+});
+app.get('/availability/:listing_id', (req, res) => {
+  axios.get(`http://ec2-3-137-149-101-us-west-2.compute.amazonaws.com:80${req.url}`)
+    .then(({ data }) => {
+      console.log('availability data', data);
       res.send(data);
     })
     .catch(err => console.log(err));
